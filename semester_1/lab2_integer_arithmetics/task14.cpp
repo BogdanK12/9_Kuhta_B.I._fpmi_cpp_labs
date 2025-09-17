4c047016e417a4f2e3862f5cbefdf59c3c1c33c2
BogdanK12 <nadgobshpion002@gmail.com>
2025-09-17 15:46:14 +0300

    Added task 14

---
+++ semester_1/lab2_integer_arithmetics/task14.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+int power(int h, int n){ // Ступені 10
+    int q = 1;
+    for(int j = 1; j <= h; j++){
+            q *= n;
+    }
+    return q;
+}
+
+int paradak(int n){ // Вызначаем колькасьць лічбаў у ліку
+    int j;
+    for(int i = 9; i >= 0; i -= 1){
+        if(n % power(i - 1, 10) != n){
+            j = i;
+            break;
+        }
+    }
+    return j;    
+} 
+
+bool praverka(int n){ // Правяраем, ці з'яўляецца лік лікам Армстронга
+    int suma = 0;
+    int roznasc = 0;
+    int bufer = n;
+    for(int i = paradak(n); i >= 0; i--){
+        suma += power(paradak(bufer), n / power(i - 1, 10));
+        n -= (n / power(i - 1, 10))*power(i - 1, 10);
+    }
+    if(suma == bufer){
+        return true;
+    } else return false;
+}
+
+int main(){
+    int a, b;
+    if(!(std::cin >> a >> b)){
+        std::cout << "Only integers, please";
+        std::exit(1);
+    }
+    while((a <= 0)||(b <= 0)){
+	std::cout << "Integers must be positive. Write down new a and b: ";
+	std::cin >> a >> b;
+	}
+    for(int i = a; i <= b; i++){
+        if(praverka(i)){
+            std::cout << i << " ";
+        }
+    }
+    std::cin.clear(); 
+    std::cin.ignore(32767, '\n');
+    std::cin.get();
+    return 0;
+}
