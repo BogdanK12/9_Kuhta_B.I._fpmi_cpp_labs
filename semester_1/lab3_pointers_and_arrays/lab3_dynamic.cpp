cc9fed03b7441525afb7b677c2963218c3517daa
BogdanK12 <nadgobshpion002@gmail.com>
2025-10-01 09:06:17 +0300

    Added <random> to Lab 3

--- semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
@@ -1,7 +1,145 @@
-
+#include <iostream>
+#include <random>
 // solve task with usage of
 // dymanic arrays
-int main() {
+void arrInputManual(int* arr, int n){
+    std::cout << "Write down all elements of array: ";
+    for(int i = 0; i < n; ++i){
+        if(!(std::cin >> arr[i])){
+            std::cout << "Write down only integers.";
+        std::exit(2);
+        }
+    }
+}
 
+void inputRandom(int* arr, int n, int a, int b, std::mt19937& gen){
+    std::uniform_int_distribution<int> element(a, b);
+    for(int i = 0; i < n; ++i){
+        arr[i] = element(gen);
+    }
+}
+
+int signChangeCount(int* arr, int n){
+    int signChange;
+    for(int i = 1; i < n; i++){
+        if(((arr[i] == 0)&&((abs(arr[i - 1])/(arr[i - 1]))==-1))||((arr[i - 1] == 0)&&(abs(arr[i])/(arr[i])==-1))){
+            ++signChange;
+        }else if(((abs(arr[i]))/(arr[i]))!=(((abs(arr[i - 1]))/(arr[i - 1])))){
+            ++signChange;
+        }
+    }
+    return signChange;
+}
+
+int sumBetweenZeros(int* arr, int n){
+    int firstZero;
+    int secZero;
+    int zeros = 0;
+    for(int i = 0; i < n; ++i){
+        if(arr[i] == 0){
+            if(zeros == 0){
+                firstZero = i;
+                ++zeros;
+            } else if(zeros == 1){
+                secZero = i;
+                ++zeros;
+                break;
+            }
+        }
+    }
+
+    int sumZeros = 0;
+
+    if(zeros >= 2){
+        for(int i = (firstZero + 1) ; i < secZero; ++i){
+            sumZeros += arr[i];
+        }
+    }
+    return sumZeros;
+}
+
+void Nchange(int* arr, int n, int N){
+    int alliquotCounter = 0;
+	for(int i = 0; i < n; ++i){
+		if((arr[i] % N) == 0){
+			int temp = i;
+			int tempVar = arr[i];
+			for(int c = temp; c > alliquotCounter; --c){
+				arr[c] = arr[c - 1];
+			}
+			arr[alliquotCounter] = tempVar;
+			++alliquotCounter;
+		}
+	}
+}
+
+void printArr(int* arr, int n){
+    std::cout << "New array: ";
+	for(int i = 0; i < n; ++i){
+		std::cout << arr[i] << " ";
+	}
+}
+
+int main() {
+    int n;
+    int how;
+    std::cout << "1 - manualy writen array. 0 - random array. ";
+
+    switch(how){
+        case 1:
+            {
+                std::cout << "How many elements does your array have: ";
+                if((!(std::cin >> n))||(n < 0)){
+                    std::cout << "Write down only positive integers.";
+                    std::exit(1);
+                }
+                int *arr = new int[n];
+                arrInputManual(arr, n);
+                std::cout << "Integers change sign " << signChangeCount(arr, n) << " times." << std::endl;
+                std::cout << "Sum of elements between first and second zero elements is: " << sumBetweenZeros(arr, n) << "." << std::endl;
+                int N;
+                std::cout << "Write down N(not equal to 0): ";
+                if((!(std::cin >> N))||(N == 0)){
+                    std::cout << "Write down only integers. Dont write 0. ";
+                    std::exit(2);
+                }
+                Nchange(arr, n, N);
+                printArr(arr,n);
+            }
+        case 2:
+            {
+                std::mt19937 gen(45218965);
+                std::uniform_int_distribution<int> dist(1, 10000);
+                int n = dist(gen);
+                int *arr = new int[n];
+                std::cout << "Write down what can be minimal and maximal elements of array: ";
+                int a, b;
+                if((!(std::cin >> a >> b))||(a >= b)){
+                    std::cout << "Write down only integers. Dont write 0. b must be greater than a. ";
+                    std::exit(2);
+                }
+                inputRandom(arr, n, a ,b, gen);
+                std::cout << "Integers change sign " << signChangeCount(arr, n) << " times." << std::endl;
+                std::cout << "Sum of elements between first and second zero elements is: " << sumBetweenZeros(arr, n) << "." << std::endl;
+                int N;
+                std::cout << "Write down N(not equal to 0): ";
+                if((!(std::cin >> N))||(N == 0)){
+                    std::cout << "Write down only integers. Dont write 0. ";
+                    std::exit(2);
+                }
+                Nchange(arr, n, N);
+                printArr(arr,n);
+            }
+        default:
+            {
+                std::cout << "You are stupid. ";
+                std::exit(3);
+            }
+    }    
+    
+    std::cin.clear(); 
+    std::cin.ignore(32767, '\n');
+    std::cin.get();
+    
     return 0;
 }
--- semester_1/lab3_pointers_and_arrays/lab3_static.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_static.cpp
@@ -1,33 +1,137 @@
 #include <iostream>
+#include <random>
 
-int main(){
-    const int nmax = 10;
-    int change = 0;
-    int n;
-    std::cout << "Write down number of elements of array(max is 10): ";
-    if(!(std::cin >> n)){
-        std::cout << "Only positive integers.";
-        std::exit(1);
-    }
-    if((n > nmax)||(n <= 0)){
-        std::exit(1);
-    }
-    int a[n];
+void arrInputManual(int* arr, int n){
     std::cout << "Write down all elements of array: ";
-    for(int i = 0; i <= (n - 1); i++){
-        std::cin >> a[i];
+    for(int i = 0; i < n; ++i){
+        if(!(std::cin >> arr[i])){
+            std::cout << "Write down only integers.";
+        std::exit(2);
+        }
     }
+}
 
-    for(int i = 1; i <= (n - 1); i++){
-        if((a[i]/abs(a[i])) != (a[i - 1]/abs(a[i - 1]))){
-            change += 1;
+void inputRandom(int* arr, int n, int a, int b, std::mt19937& gen){
+    std::uniform_int_distribution<int> element(a, b);
+    for(int i = 0; i < n; ++i){
+        arr[i] = element(gen);
+    }
+}
+
+int productAfterBig(int* arrNum, int n){
+    int maxElement = 0;
+    int indexMaxElement = 0;
+
+    for(int i = 0; i <= (n - 1); ++i){
+        if(abs(arrNum[i]) > maxElement){
+            maxElement = arrNum[i];
+            indexMaxElement = i;
         }
     }
 
-    std::cout << "Numbers change sign " << change << " times."; //eshkere
+    int product = 1;
+
+    for(int i = (indexMaxElement + 1); i <= (n - 1); ++i){
+        product *= arrNum[i];
+    }
+
+    if(indexMaxElement == (n - 1)){
+        product = 0;
+    }
+    return product;
+}
+
+int Pcounter(int *arrNum, int n, int P){
+    int number;
+    for(int i = 0; i <= (n - 1); ++i){
+        if(arrNum[i] > P){
+            ++number;
+        }
+    }
+    return number;
+}
+
+void negativeChange(int* arrNum, int n){
+    int negativeCounter = 0;
+	for(int i = 0; i < n; ++i){
+		if(arrNum[i] < 0){
+			int temp = i;
+			int tempVar = arrNum[i];
+			for(int c = temp; c > negativeCounter; --c){
+				arrNum[c] = arrNum[c - 1];
+			}
+			arrNum[negativeCounter] = tempVar;
+			++negativeCounter;
+		}
+	}
+}
+
+void printArr(int* arr, int n){
+    std::cout << "New array: ";
+	for(int i = 0; i < n; ++i){
+		std::cout << arr[i] << " ";
+	}
+}
+
+int main(){
+    int nmax = 1000;
+    int arrNum[nmax];
+    int n;
+    int number;
+    int P;
+    std::cout << "Write down P: ";
+    
+    if(!(std::cin >> P)){
+        std::cout << "Write down only integers.";
+        std::exit(2);
+    }
+
+    int how;
+    std::cout << "1 - manualy writen array. 0 - random array. ";
+
+    switch(how){
+        case 1:
+        {
+            std::cout << "How many elements does your array have: ";
+            if((!(std::cin >> n))||(n < 0)||(n > (nmax + 1))){
+                std::cout << "Write down only positive integers smaller than " << nmax << ".";
+                std::exit(2);
+            }
+            arrInputManual(arrNum, n);
+            std::cout << "There are " << Pcounter(arrNum, n, P) << " integers in your array, that are greater than " << P << "." << std::endl;
+            std::cout << "Product of numbers that come after the biggest is: " << productAfterBig(arrNum, n) << "." << std::endl;
+            negativeChange(arrNum, n);
+            printArr(arrNum, n);
+        }
+        case 2:
+        {
+            std::mt19937 gen(45218965);
+            std::uniform_int_distribution<int> dist(1, 10000);
+            int n = dist(gen);
+            int *arr = new int[n];
+            std::cout << "Write down what can be minimal and maximal elements of array: ";
+            int a, b;
+            if((!(std::cin >> a >> b))||(a >= b)){
+                std::cout << "Write down only integers. Dont write 0. b must be greater than a. ";
+                std::exit(2);
+            }
+            inputRandom(arr, n, a ,b, gen);
+            std::cout << "There are " << Pcounter(arrNum, n, P) << " integers in your array, that are greater than " << P << "." << std::endl;
+            std::cout << "Product of numbers that come after the biggest is: " << productAfterBig(arrNum, n) << "." << std::endl;
+            negativeChange(arr, n);
+            printArr(arr,n);
+        }
+    }
+
+    std::cout << "New array: ";
+
+	for(int i = 0; i < n; ++i){
+		std::cout << arrNum[i] << " ";
+	}
 
     std::cin.clear(); 
     std::cin.ignore(32767, '\n');
     std::cin.get();
+
     return 0;
 }
