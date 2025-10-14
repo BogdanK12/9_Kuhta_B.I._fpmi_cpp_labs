3a3e33b125a09d58447a334aff0a18ab0645e24a
BogdanK12 <nadgobshpion002@gmail.com>
2025-10-14 23:17:35 +0300

    Added Lab 5

---
+++ semester_1/lab5_strings/lab5.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+void inputString(std::string& stringInput){
+  std::cout << "Write down your string: ";
+  std::getline(std::cin, stringInput);
+}
+
+std::string sumOfStrings(std::string string1, std::string string2){
+  std::string string3;
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  while(i < string1.length() + string2.length()){
+    while((string1[j] != ' ') && (j < string1.length() + 1)){
+      string3 += string1[j];
+      ++i;
+      ++j;
+    }
+    if((i < string1.length() + string2.length()) && (j < string1.length())){
+      string3 += ' ';
+      ++j;
+    }
+    ++i;
+    while((string2[k] != ' ') && (k < string2.length())){
+      string3 += string2[k];
+      ++i;
+      ++k;
+    }
+    if((i < string1.length() + string2.length()) && (k < string2.length() + 1)){
+      string3 += ' ';
+      ++k;
+    }
+  }
+  return string3;
+}
+
+int main(){
+  std::string string1, string2;
+  inputString(string1);
+  inputString(string2);
+  std::string string3 = sumOfStrings(string1, string2);
+  std::cout << string3;
+  return 0;
+}
+;
