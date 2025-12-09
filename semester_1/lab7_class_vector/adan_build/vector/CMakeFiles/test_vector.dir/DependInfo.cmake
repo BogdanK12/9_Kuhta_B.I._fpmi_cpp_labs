8028a62955e8e9ebffc1da29205c6dd068e8a808
BogdanK12 <nadgobshpion002@gmail.com>
2025-12-09 22:00:01 +0300

    aaaa

---
+++ semester_1/lab7_class_vector/adan_build/vector/CMakeFiles/test_vector.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bbquiz/Proga/OIMP/9_Kuhta_B.I._fpmi_cpp_labs/semester_1/lab7_class_vector/vector/test.cpp" "vector/CMakeFiles/test_vector.dir/test.cpp.o" "gcc" "vector/CMakeFiles/test_vector.dir/test.cpp.o.d"
+  "/home/bbquiz/Proga/OIMP/9_Kuhta_B.I._fpmi_cpp_labs/semester_1/lab7_class_vector/vector/vector_impl.cpp" "vector/CMakeFiles/test_vector.dir/vector_impl.cpp.o" "gcc" "vector/CMakeFiles/test_vector.dir/vector_impl.cpp.o.d"
+  "" "test_vector" "gcc" "vector/CMakeFiles/test_vector.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
