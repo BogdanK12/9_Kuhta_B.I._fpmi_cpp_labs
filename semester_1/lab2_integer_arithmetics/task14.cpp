8a682d93e67c67bc6d82aa7507cb192078ef329a
BogdanK12 <nadgobshpion002@gmail.com>
2025-12-13 13:35:37 +0300

    git

--- semester_1/lab2_integer_arithmetics/task14.cpp
+++ semester_1/lab2_integer_arithmetics/task14.cpp
@@ -2,12 +2,14 @@
 
 int power(int h, int n){ // Ступені
     int q = 1;
-    for(int j = 1; j <= h; j++){
+    for(int j = 1; j <= h; j++)
+    {
             q *= n;
     }
     return q;
 }
 
+<<<<<<< HEAD
 int power_finder(int n){ // Вызначаем колькасьць лічбаў у ліку
     int j = 0;
     while (n > 0)
@@ -17,6 +19,17 @@ int power_finder(int n){ // Вызначаем колькасьць лічбаў
     }
     return j;
 }
+=======
+int paradak(int n){ // Вызначаем колькасьць лічбаў у ліку
+    int pp = 0;
+    while(n > 0)
+    {
+        n /= 10;
+        ++pp;    
+    }
+    return pp;
+} 
+>>>>>>> 538098a (aaaaaaaaaaa)
 
 bool is_armstrong(int number){ // Правяраем, ці з'яўляецца лік лікам Армстронга
     int suma = 0;
@@ -30,3 +43,36 @@ bool is_armstrong(int number){ // Правяраем, ці з'яўляецца �
         return true;
     } else return false;
 }
+<<<<<<< HEAD
+=======
+
+int main(){
+    int a, b;
+    std::cout << "Write down 2 positive integers: ";
+    if(!(std::cin >> a >> b)){
+        std::cout << "Only integers, please";
+        std::exit(1);
+    }
+
+    if(a > b){
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    if((a <= 0)||(b <= 0)){
+	    std::cout << "Integers must be positive.";
+	    std::exit(1);
+	}
+
+    std::cout << "These are Armstrong's numbers between " << a << " and " << b << ":" << std::endl;
+
+    for(int i = a; i <= b; i++){
+        if(praverka(i)){
+            std::cout << i << " ";
+        }
+    }
+    return 0;
+}
+>>>>>>> 538098a (aaaaaaaaaaa)
--- semester_1/rgr1_forward_list/forward_list/forward_list_impl.cpp
+++ semester_1/rgr1_forward_list/forward_list/forward_list_impl.cpp
@@ -1,26 +1,103 @@
+#include <cmath>
+#include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <iostream>
 #include <iterator>
+#include <regex>
 #include "forward_list_impl.h"
 
 // your code goes here
 
-  bool ForwardList::ForwardListIterator::operator==(const ForwardListIterator& other) const
-  {
+ForwardList()
+: first_(nullptr), size_(0);
+
+ForwardList::ForwardListIterator ForwardList::begin()
+{
+    ForwardListIterator retval(this->first_);
+    return retval;
+}
+
+ForwardList::ForwardListIterator ForwardList::end()
+{
+    Node* temp = this->first_;
+    while(temp->next_ != nullptr)
+    {
+        temp = temp->next_;
+    }
+    ForwardListIterator retval(temp);
+    return retval;
+}
+
+ForwardList::ForwardListIterator& ForwardList::ForwardListIterator::operator ++()
+{
+    if(position_ != nullptr)
+    {
+        position_ = position_->next_;
+    }
+    return *this;
+}
+
+ForwardList::ForwardListIterator ForwardList::ForwardListIterator::operator ++(int)
+{
+    ForwardListIterator retval = *this;
+    ++(*this);
+    return retval;
+}
+
+bool ForwardList::ForwardListIterator::operator==(const ForwardListIterator& other) const
+{
     return this->position_ == other.position_;
-  }
+}
 
-  bool ForwardList::ForwardListIterator::operator!=(const ForwardListIterator& other) const
-  {
+bool ForwardList::ForwardListIterator::operator!=(const ForwardListIterator& other) const
+{
     return !(*this == other);
-  }
+}
 
-  int32_t& ForwardList::ForwardListIterator::operator*() const
-  {
-      return position_->value_;
-  }
+int32_t& ForwardList::ForwardListIterator::operator*() const
+{
+    return position_->value_;
+}
 
-  int32_t& ForwardList::ForwardListIterator::operator&()
-  {
-      return &position->value_;
-  }
+int* ForwardList::ForwardListIterator::operator ->()
+{
+    return &position_->value_;
+}
+
+void ForwardList::push_back(int32_t value)
+{
+    Node* temp = this->first_;
+    while(temp->next_ != nullptr)
+    {
+        temp = temp->next_;
+    }
+    Node new_node(value);
+    temp->next_ = &new_node;
+}
+
+void ForwardList::push_back2(int32_t value)
+{
+    ForwardListIterator iter = this->end();
+    Node new_node(value);
+    (*iter.position_) = &new_node;
+}
+
+ForwardList(const ForwardList& rhs)
+{
+    ForwardListIterator iter = rhs.begin();
+
+}
+
+int32_t ForwardList::Front() const
+{
+    if(this->first_ != nullptr)
+    {
+        return this->first_->value_;
+    }
+}
+
+size_t ForwardList::Size() const
+{
+    return this->size_;
+}
