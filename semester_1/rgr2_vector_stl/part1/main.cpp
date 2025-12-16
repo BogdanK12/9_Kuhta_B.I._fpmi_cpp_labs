04fd8d7179daec6b0fe8406293543f960b42abf3
BogdanK12 <nadgobshpion002@gmail.com>
2025-12-16 10:50:31 +0000

    Started RGR-2

--- semester_1/rgr2_vector_stl/part1/main.cpp
+++ semester_1/rgr2_vector_stl/part1/main.cpp
@@ -1,8 +1,46 @@
+#include <cstddef>
+#include <vector>
 
+void input_vector(std::vector<int>& vect)
+{
+    int input;
+    while(!(std::cin >> input))
+    {
+        vect.push_back(input);
+    }
+}
 
+int count_equal_to(std::vector<int>& vect, int search_value)
+{
+    size_t count = 0;
+    for(int value: vect)
+    {
+        if(value == search_value)
+        {
+          ++count;  
+        }
+    }
+    return count;
+}
+
+int count_cond(std::vector<int>& vect, bool (*cond)(int a))
+{
+    size_t count = 0;
+    for(int value: vect)
+    {
+        if(cond(value))
+        {
+            ++count;
+        }
+    }
+}
 
 int main() {
 
+    std::vector<int> vect;
+    input_vector(vect);
+    int count = vect.size();
+    
     // your code goes here
 
     return 0;
